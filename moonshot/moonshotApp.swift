448b40ff9f2acdd587c573ee576dabefaad45675
Iresharma <iersh.sharma8@gmail.com>
2021-07-05 04:41:04 +0530

    Initial Commit

---
+++ moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by iresh sharma on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
